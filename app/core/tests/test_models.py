3d1e51438c32c6ff535595f24846c3085e5bda05
berik.sarman <@berik.sarman>
2023-04-10 11:13:47 +0600

    customize user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class TestModels(TestCase):
+    def test_create_user_with_email_successful(self):
+        email = 'test@local.local'
+        password = 'P@ssw0rd123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
