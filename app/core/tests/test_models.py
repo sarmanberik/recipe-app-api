e78a185b737247f95464e0f072d987aa760909ab
berik.sarman <@berik.sarman>
2023-04-10 12:24:20 +0600

    value error and normalize email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,17 @@ class TestModels(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_email_normalize(self):
+        sample_emails = [
+            ['test1.@LOCAL.LOCAL', 'test1.@local.local'],
+            ['Test2.@Local.local', 'Test2.@local.local'],
+            ['TEST3.@LOCAL.LOCAL', 'TEST3.@local.local'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'P@ssw0rd')
+            self.assertEqual(user.email, expected )
+
+    def test_new_user_without_email_raise_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test')
