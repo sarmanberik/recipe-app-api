658cbe021073171dc80f63dbe7d395f2d33a23a7
berik.sarman <@berik.sarman>
2023-04-14 11:21:39 +0600

    ingredient listing api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -45,4 +46,16 @@ class TagViewSet(mixins.UpdateModelMixin,
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
